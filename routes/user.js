1e3ab1e629c308cfb271066a3cc9a10b4c93f64b
light04sk <skmr39630@gmail.com>
2025-09-18 23:12:49 +0530

    Add login route and authenticate valid user

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("./users/signup.ejs");
@@ -24,5 +25,19 @@ router.post(
   })
 );
 
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    res.redirect("/listings");
+  }
+);
 
 module.exports = router;
